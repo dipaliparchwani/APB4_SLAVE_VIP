4d0648b19068694f186feb02a47465c5ce1dd32b
dipali <Dipali.Parchwani@asicraft-tech.com>
2025-04-25 19:22:32 +0530

    unnecessary signals removed

--- SV/transaction.sv
+++ SV/transaction.sv
@@ -1,21 +1,20 @@
-class transaction #(parameter int addr_width = 32,data_width = 32);
-	randc bit PSEL;
-	randc bit PWRITE;
-	randc bit PENABLE;
-	rand bit [addr_width-1:0] PADDR;
-	rand bit [data_width-1:0] PWDATA;
-	randc bit [data_width/8] PSTRB;
-	bit [data_width-1:0]PRDATA;
-	bit PREADY;
-	bit PSLVERR;
-	constraint addr_control {PADDR inside {[0:1023]};}
-	//constraint 
-	function void display();
-		$display("PSEL =%b,PWRITE = %b, PENABLE = %b, PADDR = %b, PWDATA = %b, PSTRB = %b, PRDATA = %b, PREADY = %b, PSLVERR = %b",PSEL,PWRITE,PENABLE,PADDR,PWDATA,PSTRB,PRDATA,PREADY,PSLVERR,$time);
-	endfunction
-	constraint c1 {PSEL == 1'b1; PENABLE == 1'b1; 
-        PWRITE == 0 -> PSTRB == 0;
-	}
+class transaction #(parameter int addr_width = 32,data_width = 32); //parameterized data and address width
+  rand bit PWRITE;
+  bit PSLVERR;
+  rand bit [addr_width-1:0] PADDR;
+  rand bit [data_width-1:0] PWDATA;
+  bit [data_width-1:0] PRDATA;
+  rand bit [data_width/8] PSTRB;
+  rand bit wait_state;
+  rand int no_of_wait_cycles;
+
+  constraint addr_control{PADDR inside {[0:1023]};} //for valid address generation
+  constraint strb_control{PWRITE == 0 -> PSTRB == 0;} //when pwrite low than pstrb must low as per spec
+  constraint wait_control{if(wait_state) no_of_wait_cycles inside {[0:10]}; else no_of_wait_cycles == 0;}
 
 
+  function void display();
+    $info("[%0t] : PWRITE = %0h,PSLVERR = %0h,PADDR = %0h,PWDATA = %0h,PRDATA = %0h,PSTRB = %0h",$time,PWRITE,PSLVERR,PADDR,PWDATA,PRDATA,PSTRB);
+  endfunction
 endclass
+
