8b7ee9dee75fa36df0ffb6e57eab2c5e2005c41e
dipali <Dipali.Parchwani@asicraft-tech.com>
2025-04-25 19:17:40 +0530

    wait state support for pready added

--- SV/configure.sv
+++ SV/configure.sv
@@ -1,4 +1,5 @@
 class configure #(parameter int depth = 1024,width = 8);
-	bit [width-1:0] memory [depth-1:0]; 
-
+  bit [width-1:0] memory [depth-1:0];
+  bit wait_state; //if you want wait state for PREADY rhen make it high
+  int no_of_wait_cycles; // if you want wait state then set the no_of_wait_cycles
 endclass
