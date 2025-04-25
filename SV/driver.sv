1b40de7b6594a6b32437a22fb8c4efc1af5eff48
dipali <Dipali.Parchwani@asicraft-tech.com>
2025-04-25 19:23:33 +0530

    driver implemented as fsm logic

--- SV/driver.sv
+++ SV/driver.sv
@@ -1,65 +1,115 @@
-`include "configure.sv"
+`include "configure.sv" //import package to include config file
+//`include "transaction.sv" //import package to include transaction file import at test and tb
 class driver;
+  configure c1;
+ // transaction t1;
+ // mailbox gen2drv;
+  typedef enum logic [1:0]{IDLE,SETUP,ACCESS}slave_state;
+  slave_state ps;
 
-	configure c1;
-	int i,j = 0,count;
-	virtual apb_slave_if vif;
+  virtual apb_slave_if vif; //taking interface handle as virtual because class is dynamic and interface is static to bind static with dynamic
 
-	function new(virtual apb_slave_if vif);
-		this.vif = vif;
-		count = 0;
-		c1 = new();
-	endfunction
+  function new(virtual apb_slave_if vif,mailbox gen2drv);
+    this.vif = vif;
+    this.gen2drv = gen2drv;
+    c1 = new();
+    t1 = new();
+    ps = IDLE;
+  endfunction
 
-	task run();
-		forever begin
-			@(negedge vif.PCLK) begin
-			
-				// this logic for if we receive random values
-				// from master
-			/*	if(vif.PRESETn) begin
-				vif.PSEL <= 0;
-				vif.PENABLE <= 0;
-				vif.PWDATA <= 0;
-				vif.PWRITE <= 0;
-				vif.PADDR <= 0;
-				end*/
-				
-				if(!(vif.PSEL && vif.PENABLE)) begin
-					vif.PREADY <= 0;
-					vif.PSLVERR <= 0;
-				end
 
-			wait(vif.PSEL == 1'b1);
-			wait(vif.PENABLE == 1'b1);
-			vif.PREADY <= 1'b1;
 
-			if(vif.PWRITE) begin
-				foreach(vif.PSTRB[i]) begin
-					
-					if(vif.PSTRB[i] == 1'b1) begin
+  task run();
+   // wait(vif.PRESETn == 1'b1);
+   // ps = IDLE;
+    forever begin
+      @(posedge vif.PCLK);
+     // gen2drv.get(t1);
+      if(!vif.PRESETn)begin
+	vif.PREADY <= 0;
+        vif.PRDATA <= 0;
+	vif.PSLVERR <= 0;
+	ps = IDLE;
+      end
+      else begin
+	case(ps)
+	  IDLE : begin
+            vif.PREADY <= $urandom;
+	    if(vif.PSEL)
+	      ps <= SETUP;
+            else
+	      ps <= IDLE;
+            $display("[%0t] : vif.PSEL = %0h,vif.PENABLE = %0h,vif.PREADY = %0h",$time,vif.PSEL,vif.PENABLE,vif.PREADY);
+            t1.display();
 
-						c1.memory[vif.PADDR+j] <= vif.PWDATA[(8*i)+7 -: 8];
-						j++;
-					end
-				end
-			end
-			else
-				vif.PRDATA <= c1.memory[vif.PADDR];
-			if(vif.PADDR >1023) vif.PSLVERR <= 1'b1;
-			else vif.PSLVERR <= 1'b0;
-		
-			$display("[drv] : data send to interface %0t",$time);
-			count++;
-			$display("PSEL =%b,PWRITE = %b, PENABLE = %b, PADDR = %b, PWDATA = %b, PSTRB = %b, PRDATA = %b, PREADY = %b, PSLVERR = %b,count = %0d",vif.PSEL,vif.PWRITE,vif.PENABLE,vif.PADDR,vif.PWDATA,vif.PSTRB,vif.PRDATA,vif.PREADY,vif.PSLVERR,count);
-		//	$display(memory = %p",c1.memory);
+          end
 
-	
+	  SETUP : begin
+            vif.PREADY <= $urandom;
+	    if(vif.PSEL && vif.PENABLE)
+	      ps <= ACCESS;
+            else if(vif.PSEL && !vif.PENABLE)
+	      ps <= SETUP;
+            else
+	      ps <= IDLE;
+            $display("[%0t] : vif.PSEL = %0h,vif.PENABLE = %0h,vif.PREADY = %0h",$time,vif.PSEL,vif.PENABLE,vif.PREADY);
+            t1.display();
 
-		end
-	end
-	endtask
-endclass	
-        		
-                                                   
-        
+          end
+
+	  ACCESS : begin
+	    if(vif.PSEL && vif.PENABLE)begin
+	      if(c1.wait_state)begin
+		vif.PREADY <= 0;
+		repeat(c1.no_of_wait_cycles);
+	      end
+	      vif.PREADY <= 1'b1;
+
+	      if(vif.PADDR>c1.depth)
+		vif.PSLVERR <= 1'b1;
+	      else
+		vif.PSLVERR <= 1'b0;
+	      if(vif.PWRITE)begin
+		c1.memory[vif.PADDR] <= vif.PWDATA;
+		if(vif.PSEL)
+		  ps <= SETUP;
+	        else
+	          ps <= IDLE;
+	      end
+	      else begin
+		vif.PRDATA <= c1.memory[vif.PADDR];
+		if(vif.PSEL)
+		  ps <= SETUP;
+	        else
+		  ps <= IDLE;
+	      end
+	    end
+	    else if(vif.PSEL && !vif.PENABLE)
+	      ps <= SETUP;
+            else
+	      ps <= IDLE;
+	    
+	    $display("[%0t] : vif.PSEL = %0h,vif.PENABLE = %0h,vif.PREADY = %0h",$time,vif.PSEL,vif.PENABLE,vif.PREADY);
+            t1.display();
+
+	  end
+
+	endcase
+      end
+      $display("[%0t] : vif.PSEL = %0h,vif.PENABLE = %0h,vif.PREADY = %0h",$time,vif.PSEL,vif.PENABLE,vif.PREADY);
+      t1.display();
+    end
+  endtask
+endclass
+
+ 
+
+         
+
+
+
+
+	    
+	    
+
+
