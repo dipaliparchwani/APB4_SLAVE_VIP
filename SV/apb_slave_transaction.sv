851c6895f1f738417318027b8aa25e582a550754
dipali <Dipali.Parchwani@asicraft-tech.com>
2025-04-30 18:27:06 +0530

    addr and data with declared using define

--- SV/apb_slave_transaction.sv
+++ SV/apb_slave_transaction.sv
@@ -1,28 +1,20 @@
 //----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//----file name : transaction.sv
-//----Developer : dipali
+//----file name : apb_slave_transaction.sv
 //----Description : this class consist some signals of APB and some constarints 
 //----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-//`include "apb_slave_config.sv"
-class apb_slave_transaction #(parameter int addr_width = 32,data_width = 32); //parameterized data and address width
+class apb_slave_transaction; //centralized addr_width and data_width provided using `define in sv_package file 
   rand bit PWRITE;
-  apb_slave_config c1;
   bit PSLVERR;
-  rand bit [addr_width-1:0] PADDR;
-  rand bit [data_width-1:0] PWDATA;
-  bit [data_width-1:0] PRDATA;
-  rand bit [data_width/8] PSTRB;
-  function new();
-    c1 = new();
-  endfunction
+  rand bit [`addr_width-1:0] PADDR;
+  rand bit [`data_width-1:0] PWDATA;
+  bit [`data_width-1:0] PRDATA;
+  rand bit [`data_width/8] PSTRB;
 
   constraint addr_control{PADDR inside {[0:1023]};} //for valid address generation
   constraint strb_control{PWRITE == 0 -> PSTRB == 0;} //when pwrite low than pstrb must low as per spec
-  constraint wait_control{if(c1.wait_state) c1.no_of_wait_cycles inside {[0:10]}; else c1.no_of_wait_cycles == 0;}
 
-
-  function void display();
+  function void display(); //---for display values  
     $info("[%0t] : PWRITE = %0h,PSLVERR = %0h,PADDR = %0h,PWDATA = %0h,PRDATA = %0h,PSTRB = %0h",$time,PWRITE,PSLVERR,PADDR,PWDATA,PRDATA,PSTRB);
   endfunction
 endclass
--- SV/sv_package.sv
+++ SV/sv_package.sv
@@ -1,5 +1,7 @@
 package file_pkg;
-  `include "apb_slave_config.sv"
+  `define addr_width 32 // `define compiler directive hence we can not change at runtime
+  `define data_width 32 // it perform global substitution it remain active for all files compiled after macro definition
+  `include "apb_slave_config.sv"  // `include copy the content of one file to another during compilation
   `include "apb_slave_transaction.sv"
   `include "apb_slave_generator.sv"
   `include "apb_slave_driver.sv"
