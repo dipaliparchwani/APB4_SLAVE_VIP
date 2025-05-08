0d1ce7537d5a534b7a6bf14beb00af203bee759f
dipali <Dipali.Parchwani@asicraft-tech.com>
2025-05-08 19:10:31 +0530

    modified as general clock support added

--- top/tb.sv
+++ top/tb.sv
@@ -1,31 +1,47 @@
 `include "../SV/interface.sv"
-import file_pkg::*;
-import test_pkg::*; 
+import file_pkg::*;     //SV files Package
+import test_pkg::*;     //test file Package
+
 module tb;
-logic PCLK;
-logic PRESETn;
-apb_slave_if vif(.PCLK(PCLK),.PRESETn(PRESETn));
-test tst;
-initial begin
-  tst = new(vif);
-  tst.test_run();
-end
 
-initial begin
-  PCLK = 0;
-end
+  logic PCLK = 0;
+  logic PRESETn;
+  real freq;      //frequency is supported in MHz
+  real clk_period;
 
-initial begin
-  #1 PRESETn = 0;
-  #30 PRESETn = 1;
-end
+  apb_slave_if vif(.PCLK(PCLK), .PRESETn(PRESETn));
+  test tst;
 
-always #10 PCLK  = ~PCLK;
+  // Set up frequency and clk_period at runtime
+  initial begin
+    if ($value$plusargs("freq=%0f", freq)) 
+    clk_period = ((1/(freq * 1e6)) * 1e9);
+    $info("freq = %0.2f MHz, clk_period = %0.2f ns", freq, clk_period);
+  end
 
-initial begin
-  $dumpfile("slave.vcd");
-  $dumpvars(0,tb);
-end
+  // Clock generation
+  initial begin
+    wait (clk_period > 0); // ensure clk_period is valid
+    forever #(clk_period / 2) PCLK = ~PCLK;     //clk designed based on frequency because we don't know APB Master Frequency
+  end
+
+  // Reset logic
+  initial begin
+    PRESETn = 0;
+    #30 PRESETn = 1;
+  end
+
+  // Test execution
+  initial begin
+    tst = new(vif);
+    tst.test_run();
+  end
+
+  // VCD dump
+  initial begin
+    $dumpfile("slave.vcd");
+    $dumpvars(0, tb);
+  end
 
 endmodule
 
